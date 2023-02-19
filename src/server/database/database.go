cf15d79b2ca7dd88fb2aa8a3c4855f24d9fb1519
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-02-18 22:49:40 -0500

    Merged/added logic for Redis caching db initialization to database and config code.

--- src/server/database/database.go
+++ src/server/database/database.go
@@ -1,15 +1,17 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"server/models"
 
+	"github.com/go-redis/redis/v7"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// TODO: Add comment documentation (func Initialize)
-func Initialize(connectionString string) *gorm.DB {
+// TODO: Add comment documentation (func InitializePostgresDB)
+func InitializePostgresDB(connectionString string) *gorm.DB {
 
 	dbInstance, dbError := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 
@@ -20,8 +22,30 @@ func Initialize(connectionString string) *gorm.DB {
 
 	log.Println("Connected to Database!")
 
-	dbInstance.AutoMigrate(&models.User{})
+	dbInstance.AutoMigrate(&models.User{},
+		&models.Address{},
+		&models.ContactInfo{},
+		&models.Business{},
+		&models.Office{},
+		&models.Service{},
+		&models.ServiceOffering{},
+		&models.Appointment{})
+
 	log.Println("Database Migration Completed!")
 
 	return dbInstance
+}
+
+// TODO: Add comment documentation (func InitializeRedisDB)
+func InitializeRedisDB(dsn string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+
+	pong, err := client.Ping().Result()
+	if err != nil {
+		fmt.Println(pong, err)
+	}
+
+	return client
 }
