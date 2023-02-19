0113ef44136e0933b7af1b8fea6af9469cd618bf
auriartew <gina.auw@gmail.com>
2023-02-18 22:49:40 -0500

    Merged/added logic for Redis caching db initialization to database and config code.

--- src/server/config/config.go
+++ src/server/config/config.go
@@ -12,14 +12,16 @@ var AppConfig *Configuration = InitializeConfig()
 
 // struct to map env values
 type Configuration struct {
-	JWT_SIGNING_KEY []byte `mapstructure:"JWT_SIGNING_KEY"`
-	APP_DB_NAME     string `mapstructure:"APP_DB_NAME"`
-	APP_DB_USER     string `mapstructure:"APP_DB_USER"`
-	APP_DB_PASSWORD string `mapstructure:"APP_DB_PASSWORD"`
-	APP_DB_HOST     string `mapstructure:"APP_DB_HOST"`
-	APP_DB_PORT     int    `mapstructure:"APP_DB_PORT"`
-	API_SERVER_HOST string `mapstructure:"API_SERVER_HOST"`
-	API_SERVER_PORT int    `mapstructure:"API_SERVER_PORT"`
+	JWT_SIGNING_KEY   []byte `mapstructure:"JWT_SIGNING_KEY"`
+	APP_DB_NAME       string `mapstructure:"APP_DB_NAME"`
+	APP_DB_USER       string `mapstructure:"APP_DB_USER"`
+	APP_DB_PASSWORD   string `mapstructure:"APP_DB_PASSWORD"`
+	APP_DB_HOST       string `mapstructure:"APP_DB_HOST"`
+	APP_DB_PORT       int    `mapstructure:"APP_DB_PORT"`
+	APP_CACHE_DB_HOST string `mapstructure:"APP_CACHE_DB_HOST"`
+	APP_CACHE_DB_PORT int    `mapstructure:"APP_CACHE_DB_PORT"`
+	API_SERVER_HOST   string `mapstructure:"API_SERVER_HOST"`
+	API_SERVER_PORT   int    `mapstructure:"API_SERVER_PORT"`
 }
 
 // Initialize method creates and initializes new Configuration object
@@ -29,7 +31,7 @@ func InitializeConfig() (config *Configuration) {
 
 // loadEnvironmentVariables reads in the variables found in the 'server/config/config.json' file and returns a struct with those variables loaded as properties
 func loadEnvironmentVariables() (config *Configuration) {
-	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
 	viper.SetConfigName("config.json")
 	viper.SetConfigType("json")
 
@@ -49,8 +51,8 @@ func (config *Configuration) GetSigningKey() []byte {
 	return config.JWT_SIGNING_KEY
 }
 
-// GetDBConnectionString returns the formatted connection string for Postgres database connections
-func (config *Configuration) GetDBConnectionString() string {
+// GetPostgresDBConnectionString returns the formatted connection string for Postgres database connections
+func (config *Configuration) GetPostgresDBConnectionString() string {
 	var connectionString string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d",
 		config.APP_DB_HOST,
 		config.APP_DB_USER,
@@ -63,9 +65,23 @@ func (config *Configuration) GetDBConnectionString() string {
 
 // GetAPIServerNetworkAddress returns the full network address ("host:port") that will serve the API
 func (config *Configuration) GetAPIServerNetworkAddress() string {
-	var networkAddress string = fmt.Sprintf("%s:%d",
+	return getNetworkAddress(
 		config.API_SERVER_HOST,
 		config.API_SERVER_PORT)
+}
+
+// GetRedisDBNetworkAddress returns DSN for Redis DB in "host:port" string format
+func (config *Configuration) GetRedisDBNetworkAddress() string {
+	return getNetworkAddress(
+		config.API_SERVER_HOST,
+		config.API_SERVER_PORT)
+}
+
+// getNetworkAddress takes in host address and port number and returns the network address (a.k.a. DSN) in "host:port" string format
+func getNetworkAddress(host string, port int) string {
+	var networkAddress string = fmt.Sprintf("%s:%d",
+		host,
+		port)
 
 	return networkAddress
 }
