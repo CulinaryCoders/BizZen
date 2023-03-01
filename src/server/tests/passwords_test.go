676faf11f53d618769a15d4af976580dac67caa6
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-03-01 13:20:05 -0500

    Moved models_test.go to 'tests' package and renamed to 'passwords_test.go' since it currently is only testing password functions. Updated the imports for that file to include the 'server/models' package and updated the 'models' functions/objects with 'models.' so that tests still function properly.

---
+++ src/server/tests/passwords_test.go
@@ -0,0 +1,45 @@
+package tests
+
+import (
+	"server/models"
+	"testing"
+)
+
+func TestHashPassword(t *testing.T) {
+	user := &models.User{Password: "password"}
+
+	err := user.HashPassword(user.Password)
+
+	// assert that the function returns no error
+	if err != nil {
+		t.Errorf("expected no error, but got %v", err)
+	}
+
+	// assert that the password is not empty
+	if user.Password == "" {
+		t.Errorf("expected hashed password to not be empty, but got an empty string")
+	}
+
+	// assert that the hashed password is different from the original password
+	if user.Password == "password" {
+		t.Errorf("expected hashed password to be different from the original password, but they are the same")
+	}
+}
+
+func TestCheckPassword(t *testing.T) {
+	user := &models.User{
+		Password: "$2a$14$yYPvfXdM7SaAdxA5jQr4Bu1jq9AsqBSA4lL.8LI8FostoL1UCcth2",
+	}
+
+	// Test with correct password
+	err := user.CheckPassword("password123")
+	if err != nil {
+		t.Errorf("CheckPassword() returned an unexpected error: %v", err)
+	}
+
+	// Test with incorrect password
+	err = user.CheckPassword("wrongpassword")
+	if err == nil {
+		t.Error("CheckPassword() did not return an error with an incorrect password")
+	}
+}
