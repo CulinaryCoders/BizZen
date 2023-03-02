eb3503200e6659f7b638995b22440bc062715ad5
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-03-01 21:26:40 -0500

    Added line to format all tables in production database before running the application. Will likely remove this in the future, but it's more helpful to start with a fresh DB at this stage of the project.

--- src/server/main.go
+++ src/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"server/config"
+	"server/database"
 	"server/handlers"
 )
 
@@ -9,5 +10,6 @@ func main() {
 	var prodDBName string = config.AppConfig.APP_DB_NAME
 
 	handlers.App.Initialize(prodDBName)
+	database.FormatAllTables(handlers.App.AppDB)
 	handlers.App.Run(config.AppConfig.GetAPIServerNetworkAddress())
 }
