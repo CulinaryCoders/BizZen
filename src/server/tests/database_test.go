4b66d9cf533c87c2bcba23ee03829d1d829b0946
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-03-29 18:41:15 -0400

    Organized unit tests and created placeholder tests for future database objects that will be implemented.

---
+++ src/server/tests/database_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	_ "server/database"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO:  Add documentation (func TestGetBusiness)
+func TestFormatAllTables(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestGetBusiness)
+func TestSetupTables(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestGetBusiness)
+func TestDropAllTables(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestGetBusiness)
+func TestGetListOfDBTables(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestGetBusiness)
+func TestInitializePostgresDB(t *testing.T) {
+	assert.True(t, true)
+}
---
+++ src/server/tests/models_operating_hours_test.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	_ "server/database"
+	_ "server/models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO:  Add documentation (func TestCreateAppointment)
+func TestCreateAppointment(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestGetAppointment)
+func TestGetAppointment(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestUpdateAppointment)
+func TestUpdateAppointment(t *testing.T) {
+	assert.True(t, true)
+}
+
+// TODO:  Add documentation (func TestDeleteAppointment)
+func TestDeleteAppointment(t *testing.T) {
+	assert.True(t, true)
+}
