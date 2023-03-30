45a037bb78aa7138f1e79c9ffb2a4798d4577df0
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-03-29 21:33:42 -0400

    Updated reference to 'DEBUG_MODE' config attribute in 'main_test.go' program within 'tests' package to new 'Debug' global variable from the 'config' package.

--- src/server/tests/main_test.go
+++ src/server/tests/main_test.go
@@ -14,8 +14,7 @@ var testAppDB *gorm.DB
 func TestMain(m *testing.M) {
 	var testDBName string = config.AppConfig.APP_TEST_DB_NAME
 	var dbConnectionString string = config.AppConfig.GetPostgresDBConnectionString(testDBName)
-	var debug bool = config.AppConfig.DEBUG_MODE
-	testAppDB = database.InitializePostgresDB(dbConnectionString, debug)
+	testAppDB = database.InitializePostgresDB(dbConnectionString, config.Debug)
 
 	os.Exit(m.Run())
 }
