6e0fa0a3e04e033803ad204be2c5a7b91ef73b4e
Alaine Spade <spadea@ufl.edu>
2023-04-11 17:36:20 -0400

    updated service.ts to fit with backend

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,12 +1,13 @@
-import { ServiceOffering } from "./service-offering";
-
+import {formatDate} from "@angular/common";
 export class Service {
     constructor(
         public serviceId : string,
         public name : string,
         public description : string,
-
-        public offering : ServiceOffering
+        public start_date_time: Date,
+        public length: number,
+        public capacity: number,
+        public price: number
 
     ) {}
 }
