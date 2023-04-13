5abe484aad5a92fa40b8199cd1b3533f58bb403e
lucindaq <lucinda@merl.us>
2023-04-13 22:57:34 +0200

    get all services working

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -13,12 +13,6 @@ export class ServiceService {
   private getAllServices = 'http://localhost:8080/services';
 
   // Adds service to DB with specified properties
-  // addService(name : string, desc : string, start_date_time: Date, length: number, capacity: number, price: number) : Promise<Service>{
-  //   return this.http.post<Service>(this.apiUrl, {
-  //     name: name, desc: desc, start_date_time: start_date_time, length: length, capacity: capacity, price: price
-  //   }).toPromise().then();
-  // }
-
   addService(service: FormGroup) : Promise<Service>{
     return this.http.post<Service>(this.apiUrl, {
       name: service.value.name, desc: service.value.description, start_date_time: service.value.start_date_time, length: service.value.length, capacity: service.value.capacity, price: service.value.price
@@ -26,7 +20,7 @@ export class ServiceService {
   }
 
   // Gets all services in db
-  getServices() : Promise<Service>{
-    return this.http.get<Service>(this.getAllServices).toPromise().then();
+  getServices() : Promise<Service[]>{
+    return this.http.get<Service[]>(this.getAllServices).toPromise().then();
   }
 }
