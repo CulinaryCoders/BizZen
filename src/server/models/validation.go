2cdaf985fb8025efc9b4355233e362746fbf43d6
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-04-17 19:37:43 -0400

    Added docstrings to 'validation.go' in 'models' package.

--- src/server/models/validation.go
+++ src/server/models/validation.go
@@ -13,39 +13,154 @@ var titleCaser cases.Caser = cases.Title(language.English)
 
 /*  --  GENERIC STANDARDIZATION FUNCTIONS  --  */
 
-// TODO:  Add documentation (func TrimWhitespace)
+/*
+*Description*
+
+func TrimWhitespace
+
+Removes leading and trailing whitespace from string.
+
+*Parameters*
+
+	    value <string>
+
+			The original string value to be trimmed.
+
+*Returns*
+
+	    _ <string>
+
+			The updated string value with leading/trailing whitespace removed.
+*/
 func TrimWhitespace(value string) string {
 	return strings.TrimSpace(value)
 }
 
-// TODO:  Add documentation (func StandardizeEmailAddress)
+/*
+*Description*
+
+func StandardizeEmailAddress
+
+Standardizes an email address string by converting it to lower case and removing trailing/leading whitespace.
+
+*Parameters*
+
+	    emailAddress <string>
+
+			The email address string to be standardized.
+
+*Returns*
+
+	    _ <string>
+
+			The updated string value that has been standardized.
+*/
 func StandardizeEmailAddress(emailAddress string) string {
 	return strings.ToLower(TrimWhitespace(emailAddress))
 }
 
-// TODO:  Add documentation (func StandardizeEmailAddress)
+/*
+*Description*
+
+func StandardizeNameField
+
+Standardizes a name string by converting it to title case and removing trailing/leading whitespace.
+
+*Parameters*
+
+	    name <string>
+
+			The name string to be standardized.
+
+*Returns*
+
+	    _ <string>
+
+			The updated string value that has been standardized.
+*/
 func StandardizeNameField(name string) string {
 	return titleCaser.String(TrimWhitespace(name))
 }
 
 /*  --  GENERIC VALIDATION FUNCTIONS  --  */
-// TODO:  Add documentation (func EmailAddressIsValid)
+
+/*
+*Description*
+
+func EmailAddressIsValid
+
+Checks if an email address string has a valid format.
+
+*Parameters*
+
+	    emailAddress <string>
+
+			The email address to be validated
+
+*Returns*
+
+	    _ <bool>
+
+			'true' if email address format is valid, else 'false'.
+*/
 func EmailAddressIsValid(emailAddress string) bool {
+	// TODO:  Implement function (EmailAddressIsValid)
 	return true
 }
 
 /*  --  OBJECT-SPECIFIC STANDARDIZATION FUNCTIONS  --  */
 
 /*  USER FIELDS  */
-// TODO:  Add documentation (func StandardizeUserAccountType)
+
+/*
+*Description*
+
+func StandardizeUserAccountType
+
+Standardizes the string formatting for a User's account type by converting it to title case and removing trailing/leading whitespace.
+
+*Parameters*
+
+	    userAcctType <string>
+
+			The account type string to be standardized.
+
+*Returns*
+
+	    _ <string>
+
+			The updated string value that has been standardized.
+*/
 func StandardizeUserAccountType(userAcctType string) string {
 	return titleCaser.String(strings.ToLower(TrimWhitespace(userAcctType)))
 }
 
 /*  --  OBJECT-SPECIFIC VALIDATION FUNCTIONS  --  */
 
-/*  USER FIELDS  */
-// TODO:  Add documentation (func UserAccountTypeIsValid)
+/*
+*Description*
+
+func UserAccountTypeIsValid
+
+Checks if User's account type is a valid value.
+
+Valid account types:
+  - Individual
+  - Business
+  - System
+
+*Parameters*
+
+	    userAcctType <string>
+
+			The account type to be validated.
+
+*Returns*
+
+	    _ <bool>
+
+			'true' if account type is a valid value, else 'false'.
+*/
 func UserAccountTypeIsValid(userAcctType string) bool {
 	validAccountTypes := []string{"business", "individual", "system"}
 
