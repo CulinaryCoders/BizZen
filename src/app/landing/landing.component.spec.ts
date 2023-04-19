0809ffd9638dd8850296946a9603c2d8f5194e0a
lucindaq <lucinda@merl.us>
2023-04-19 22:15:42 +0200

    added landing component tests

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {Router} from "@angular/router";
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+  let router : Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ LandingComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +24,19 @@ describe('LandingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should navigate to Register page', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.routeToRegister();
+    expect(navigateSpy).toHaveBeenCalledWith(['/register']);
+  });
+
+  it('Should navigate to Login page', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.routeToLogin();
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
   });
 });
