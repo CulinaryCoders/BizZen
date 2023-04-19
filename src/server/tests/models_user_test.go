331c6d8f4e00b4a42aea904c779e5ecdd402f346
Ryan Brooks <ryan.s.brooks2@gmail.com>
2023-04-19 19:50:38 -0400

    Implemented tests for 'User' object methods and cleaned up/added docstrings.

--- src/server/tests/models_user_test.go
+++ src/server/tests/models_user_test.go
@@ -7,18 +7,55 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO:  Add documentation (func TestAuthenticate)
-func TestAuthenticate(t *testing.T) {
-	assert.True(t, true)
+/*
+*Description*
+
+func TestStandardizeUserFields
+
+Tests the User.StandardizeFields method to confirm that fields are being standardized as expected.
+*/
+func TestStandardizeUserFields(t *testing.T) {
+
+	// Test user
+	testUser := models.User{
+		Email:       "TeST-oneTWOoneTWO@yahoo.com  ",
+		Password:    "pw123",
+		AccountType: "  useR  ",
+		FirstName:   "  jessiE",
+		LastName:    " PinKMAn ",
+	}
+
+	// Define expected values
+	expectedEmail := "test-onetwoonetwo@yahoo.com"
+	expectedPassword := "pw123"
+	expectedAccountType := "User"
+	expectedFirstName := "Jessie"
+	expectedLastName := "Pinkman"
+
+	// Standardize fields
+	testUser.StandardizeFields()
+
+	// Confirm standardized fields match expected values
+	assert.Equal(t, testUser.Email, expectedEmail, "Standardized user email (%s) should match expected email (%s).", testUser.Email, expectedEmail)
+	assert.Equal(t, testUser.Password, expectedPassword, "Standardized password (%s) should match expected password (%s).", testUser.Password, expectedPassword)
+	assert.Equal(t, testUser.AccountType, expectedAccountType, "Standardized account type (%s) should match expected account type (%s).", testUser.AccountType, expectedAccountType)
+	assert.Equal(t, testUser.FirstName, expectedFirstName, "Standardized first name (%s) should match expected first name (%s).", testUser.FirstName, expectedFirstName)
+	assert.Equal(t, testUser.LastName, expectedLastName, "Standardized last name (%s) should match expected last name (%s).", testUser.LastName, expectedLastName)
 }
 
-// TODO:  Add documentation (func TestCreateUser)
-func TestCreateUser(t *testing.T) {
+/*
+*Description*
+
+func TestCreateGetUser
+
+Tests the Create and Get methods for the User db object. Confirms that the created User object is returned when the method is called and that the record is created in the application  database.
+*/
+func TestCreateGetUser(t *testing.T) {
 	// Refresh database to control testing environment
 	models.FormatAllTables(testAppDB)
 
 	// Create user record
-	testCreateUser := models.User{
+	testCreateUser := &models.User{
 		Email:       "test@gmail.com",
 		Password:    "pw123",
 		AccountType: "User",
@@ -29,7 +66,7 @@ func TestCreateUser(t *testing.T) {
 	returnRecords, err := testCreateUser.Create(testAppDB)
 	createdUser := returnRecords["user"]
 	if err != nil {
-		t.Fatalf("ERROR:  Could not create test User. %s", err)
+		t.Errorf("Could not create test User.  --  %s", err)
 	}
 
 	// Attempt to retrieve newly created user record from database
@@ -38,24 +75,110 @@ func TestCreateUser(t *testing.T) {
 	returnRecords, err = testGetUser.Get(testAppDB, createdUser.GetID())
 	returnedUser := returnRecords["user"]
 	if err != nil {
-		t.Fatalf("ERROR:  func GetUser failed to return test User. %s", err)
+		t.Errorf("func GetUser failed to return test User  --  %s", err)
 	}
 
 	unequalFields, equal := models.Equal(createdUser, returnedUser)
-	assert.Truef(t, equal, "ERROR: The following fields did not match between the created and returned object  --  %s", unequalFields)
+	assert.Truef(t, equal, "The following fields did not match between the created and returned object  --  %s", unequalFields)
 }
 
-// TODO:  Add documentation (func TestGetUser)
-func TestGetUser(t *testing.T) {
-	assert.True(t, true)
-}
+/*
+*Description*
 
-// TODO:  Add documentation (func TestUpdateUser)
+func TestUpdateUser
+
+Tests the Update method for the User db object. Confirmed that the updated User object is returned and that the record was updated in the datbas. Throws the appropriate error if the record doesn't exist in the database
+*/
 func TestUpdateUser(t *testing.T) {
-	assert.True(t, true)
+	// Refresh database to control testing environment
+	models.FormatAllTables(testAppDB)
+
+	// Create user record
+	testUser := &models.User{
+		Email:       "MakingMyWayDowntown123@hotmail.com",
+		Password:    "Walking_fast",
+		AccountType: "Business",
+		FirstName:   "Vanessa",
+		LastName:    "Carlton",
+	}
+
+	returnRecords, err := testUser.Create(testAppDB)
+	createdUser := returnRecords["user"]
+	if err != nil {
+		t.Errorf("Could not create test User.  --  %s", err)
+	}
+
+	userID := createdUser.GetID()
+
+	// Attempt to update created User record
+	testUpdateUser := models.User{}
+
+	updates := map[string]interface{}{
+		"email":      "AndNowIWonder90210@uphoenix.edu",
+		"first_name": "Definitely not Vanessa",
+	}
+
+	returnRecords, err = testUpdateUser.Update(testAppDB, userID, updates)
+	updatedUser := returnRecords["user"]
+	if err != nil {
+		t.Errorf("Could not update test User.  --  %s", err)
+	}
+
+	// Attempt to retrieve created/updated user record from database
+	testGetUser := models.User{}
+
+	returnRecords, err = testGetUser.Get(testAppDB, userID)
+	returnedUser := returnRecords["user"]
+	if err != nil {
+		t.Errorf("Could not retrieve created/updated test User record from teh database.  --  %s", err)
+	}
+
+	unequalFields, equal := models.Equal(updatedUser, returnedUser)
+	assert.Truef(t, equal, "The following fields did not match between the updated and returned object  --  %s", unequalFields)
 }
 
-// TODO:  Add documentation (func TestDeleteUser)
+/*
+*Description*
+
+func TestDeleteUser
+
+Tests the Delete method for the User db object. Confirms that the deleted User object is returned when the method is called and that the record is deleted from the DB. Throws the appropriate error if the record doesn't exist.
+*/
 func TestDeleteUser(t *testing.T) {
-	assert.True(t, true)
+	// Refresh database to control testing environment
+	models.FormatAllTables(testAppDB)
+
+	// Create user record
+	testUser := &models.User{
+		Email:       "MakingMyWayDowntown123@hotmail.com",
+		Password:    "Walking_fast",
+		AccountType: "Business",
+		FirstName:   "Vanessa",
+		LastName:    "Carlton",
+	}
+
+	returnRecords, err := testUser.Create(testAppDB)
+	createdUser := returnRecords["user"]
+	if err != nil {
+		t.Errorf("Could not create test User.  --  %s", err)
+	}
+
+	userID := createdUser.GetID()
+
+	// Attempt to delete the User record that was created in the database
+	testDeleteUser := models.User{}
+
+	_, err = testDeleteUser.Delete(testAppDB, userID)
+	if err != nil {
+		t.Errorf("Could not delete test User.  --  %s", err)
+	}
+
+	// Check to see if deleted record's ID still exists in the database
+	checkUser := models.User{}
+
+	deletedIDExists, err := checkUser.IDExists(testAppDB, userID)
+	if err != nil {
+		t.Errorf("Could not confirm if deleted User record's ID still exists in the database.")
+	}
+	assert.False(t, deletedIDExists, "User record's ID still exists in the database after Delete method was executed.")
 }
